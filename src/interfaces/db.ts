ad4c0b01459e20abc35cc25ea931c63308f91782
carlosengel012 <carlosengeloc@gmail.com>
2020-08-03 16:27:44 -0400

    CRUDModel interface added

--- src/interfaces/db.ts
+++ src/interfaces/db.ts
@@ -11,3 +11,14 @@ export interface DBConnection {
     runQuery(query : string, ...params : any[]  ) : Promise<any>
 }
 
+
+export type PartialMap<T> = {
+    [P in keyof T]? : T[P]
+}
+
+export interface CRUDModel<T> {
+    find( filter? :  PartialMap<T> ) : Promise<T[]>
+    create() : Promise<void>
+    update(filter : PartialMap<T> , data : T) : Promise<T>
+    delete(filter : PartialMap<T> ) : Promise<void>
+}
